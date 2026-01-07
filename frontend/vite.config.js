1845741e97e7910e9b538270b96a18988b5a107b
Raghav Daga <r.daga@mybpp.com>
2026-01-07 16:51:22 +0000

    Refine frontend test configs for better CI compatibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default {
   plugins: [sveltekit()],
-  test: { 
-    environment: 'node'
-  },
   server: {
-    middlewareMode: true,
-    hmr: false
+    middlewareMode: true
   }
 };
